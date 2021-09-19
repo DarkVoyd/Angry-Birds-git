ed19b01322fd78a63367e64dc6388cded52b50f7
DarkVoyd <puneet.sharmafacs@gmail.com>
2021-09-19 09:59:17 +0530

    apiCalls

--- sketch.js
+++ sketch.js
@@ -11,7 +11,8 @@ var bird, slingshot;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    //backgroundImg = loadImage("sprites/bg.png");
+    getTime();
 }
 
 function setup(){
@@ -45,7 +46,8 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if (backgroundImg)
+        background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -85,4 +87,22 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getTime() {
+    var response = await fetch('https://worldtimeapi.org/api/timezone/Asia/Kolkata');
+    var jsonResponse = await response.json();
+    var hour = jsonResponse.datetime.slice(11,13);
+    console.log(hour);
+    var bg;
+    if (hour >= 06 && hour <= 19) {
+        bg = 'sprites/bg.png';
+
+    } else {
+        bg = 'sprites/bg2.jpg';
+    }
+
+    backgroundImg = loadImage(bg);
+    
+
 }
